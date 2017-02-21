a4b4c765672c0efd63a301b6a46aabd21f5ffa1e
andrewshkovskii <andrewshkovskii@gmail.com>
2017-02-21 17:15:11 +0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="tarstream",
+    version="0.2",
+    author="zerovm, Andrey Baryshnikov",
+    author_email="https://groups.google.com/forum/#!forum/zerovm , "
+                 "ab@uploadcare.com",
+    description="Streaming TAR archive.",
+    keywords="tar tarstream stream generator",
+    url="https://github.com/andrewshkovskii/tarstream",
+)
